94821088f4e8b52a5bab2b3ec854e53d06ae03f1
clearmirror-js <kangjisu0403@gmail.com>
2022-06-15 11:33:05 +0900

    initialize project

---
+++ src/test/java/kurly2/mainprojectbackend/MainprojectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kurly2.mainprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainprojectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
